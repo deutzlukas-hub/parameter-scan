3149d9cc06d4c07f74cb84706b62063c5481ce33
Lukas <scld@leeds.ac.uk>
2022-09-02 14:53:17 +0100

    Simplified imports
    ParameterGrid class gets now imported in __init__.py and can be imported
    via from parameter_scan import  ParameterGrid.

--- parameter_scan/__init__.py
+++ parameter_scan/__init__.py
@@ -1,1 +1,4 @@
-from parameter_scan import ParameterGrid
+# A absolute path is needed here, because the sys.path 
+# only knows about the parameter_scan directory
+from parameter_scan.parameter_grid import ParameterGrid
+
